c34ff0186010c2e4fd8d8444970be26e98c8d92c
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-10-03 17:56:29 -0300

    Finalizando funcionalidades de Carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { CartService } from './../../services/domain/cart.service';
 import { API_CONFIG } from './../../config/api.config';
 import { CartItem } from './../../models/cart-item';
@@ -36,6 +37,23 @@ export class CartPage {
         },
         error => {}); 
     }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+   increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+   decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+   total() : number {
+    return this.cartService.total();
+  }  
+
+  goOn() {
+    this.navCtrl.setRoot("CategoriasPage");
   }
 
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { CartService } from './domain/cart.service';
 import { StorageService } from './storage.service';
 import { LocalUser } from './../models/local_user';
 import { API_CONFIG } from './../config/api.config';
@@ -11,7 +12,10 @@ export class AuthService {
 
     jwtHelper: JwtHelper = new JwtHelper();
 
-    constructor(public http: HttpClient, public storage: StorageService) {
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public cartService: CartService) {
 
     }
 
@@ -42,6 +46,7 @@ export class AuthService {
             email: this.jwtHelper.decodeToken(tok).sub
         };
         this.storage.setLocalUser(user);
+        this.cartService.createOrClearCart();
     }
 
     logout() {
