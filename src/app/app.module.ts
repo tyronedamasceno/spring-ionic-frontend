7d1ca2fc867aa33d44ee91d749c5b7192c4fda40
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-10-04 19:46:18 -0300

    Fazendo upload da foto para S3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ImageUtilService } from './../services/image-util.service';
 import { CartService } from './../services/domain/cart.service';
 import { StorageService } from './../services/storage.service';
 import { AuthService } from './../services/auth.service';
@@ -40,7 +41,8 @@ import { ProdutoService } from '../services/domain/produto.service';
     StorageService,
     ClienteService,
     ProdutoService,
-    CartService
+    CartService,
+    ImageUtilService
   ]
 })
 export class AppModule {}
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,11 +4,15 @@ import { ClienteDTO } from './../../models/cliente.dto';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/Rx';
+import { ImageUtilService } from '../image-util.service';
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http: HttpClient, public storage: StorageService) {
+    constructor(
+        public http: HttpClient, 
+        public storage: StorageService,
+        public imgUtilService: ImageUtilService) {
 
     }
 
@@ -34,5 +38,19 @@ export class ClienteService {
                 responseType: 'text'
             }
         );
-    } 
+    }
+    
+    uploadPicture(picture) {
+        let pictureBlob = this.imgUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`,
+            formData,
+            {
+                observe: 'response',
+                responseType: 'text'
+            }
+        );
+    }
 }
