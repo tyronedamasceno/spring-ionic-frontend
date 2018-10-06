dcc4c5e7b76c3f187161f4e6badb831686e54080
Tyrone Damasceno <tyronedamasceno@gmail.com>
2018-10-06 09:15:19 -0300

    Obtendo imagem a partir da galeria do dispositivo

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -73,6 +73,26 @@ export class ProfilePage {
      });
   }
 
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+     });
+  }
+
+
   sendPicture() {
     this.clienteService.uploadPicture(this.picture)
         .subscribe(response => {
